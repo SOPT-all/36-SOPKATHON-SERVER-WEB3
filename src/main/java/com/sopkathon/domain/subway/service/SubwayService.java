84eba1f6272a20c2526abe6ce7d12fa0dfbd29f3
koobontak <kbt82883@gmail.com>
2025-05-18 03:18:43 +0900

    [feat] 지하철역 리스트 조회 api 구현

--- src/main/java/com/sopkathon/domain/subway/service/SubwayService.java
+++ src/main/java/com/sopkathon/domain/subway/service/SubwayService.java
@@ -1,7 +1,28 @@
 package com.sopkathon.domain.subway.service;
 
+import com.sopkathon.domain.subway.dto.SubwayListRes;
+import com.sopkathon.domain.subway.entity.SubwayEntity;
+import com.sopkathon.domain.subway.repository.SubwayRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class SubwayService {
+    private final SubwayRepository subwayRepository;
+
+    @Transactional(readOnly = true)
+    public SubwayListRes getSubwayList() {
+        List<String> list = subwayRepository.findAll()
+                .stream()
+                .map(SubwayEntity::getSubwayName)
+                .collect(Collectors.toList());
+
+        return SubwayListRes.from(list);
+    }
 }
