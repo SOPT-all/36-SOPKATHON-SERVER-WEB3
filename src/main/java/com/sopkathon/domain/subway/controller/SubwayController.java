b0999a1147ea22e358df09fa818d215acc7a256b
koobontak <kbt82883@gmail.com>
2025-05-18 03:51:38 +0900

    [feat] 지하철역 검색 api 구현

--- src/main/java/com/sopkathon/domain/subway/controller/SubwayController.java
+++ src/main/java/com/sopkathon/domain/subway/controller/SubwayController.java
@@ -7,6 +7,7 @@ import com.sopkathon.global.error.dto.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,13 @@ public class SubwayController {
     @GetMapping("/subways")
     public ResponseEntity<SuccessResponse<SubwayListRes>> getSubwayList() {
         SubwayListRes subwayListRes = subwayService.getSubwayList();
+
+        return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_SUBWAY_LIST, subwayListRes));
+    }
+
+    @GetMapping("/subways/search")
+    public ResponseEntity<SuccessResponse<SubwayListRes>> getSubwayListByKeyword(@RequestParam(name = "keyword") String keyword) {
+        SubwayListRes subwayListRes = subwayService.getSubwayListByKeyword(keyword);
 
         return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_SUBWAY_LIST, subwayListRes));
     }
