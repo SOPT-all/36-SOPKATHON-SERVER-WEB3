db830cacee2a3a9e9c85975a913485f46ae8a732
woals2840 <woals2840@gmail.com>
2025-05-18 01:17:34 +0900

    [feat/#8] 엔티티 세팅

--- src/main/java/com/sopkathon/domain/subway/entity/SubwayEntity.java
+++ src/main/java/com/sopkathon/domain/subway/entity/SubwayEntity.java
@@ -1,5 +1,24 @@
 package com.sopkathon.domain.subway.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "subway")
 public class SubwayEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "subway_name", nullable = false)
+	private String subwayName;
 }
